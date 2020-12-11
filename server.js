80cc19d385d28925b30feae854af152a02a6d953
nfcarbone <nfcarbone@gmail.com>
2020-12-11 12:15:00 -0500

    better console logs

--- server.js
+++ server.js
@@ -5,33 +5,34 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.post('/api/exercise/new-user/', (req, res) => {
-  console.log(req);
+  console.log('new-user: ', req.body);
   res.end();
 });
 
 app.get('/api/exercise/users/', (req, res) => {
-  console.log(req);
+  console.log('users: ', req.body);
   res.end();
 });
 
 app.post('/api/exercise/add/', (req, res) => {
-  console.log(req);
+  console.log('add: ', req.body);
   res.end();
 });
 
 app.get('/api/exercise/log/', (req, res) => {
-  console.log(req);
+  console.log('log: ', req.body);
   res.end();
 });
 
 app.get('/api/exercise/log/:id/:from?/:to?/:limit?/', (req, res) => {
-  console.log(req.params);
+  console.log('log params: ', req.params);
   res.end();
 });
 
